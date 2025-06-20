c582f7f0efa5a9bbddfc0500906b803fed736e63
BramG08 <bgrippeling08@gmail.com>
2025-06-20 14:30:18 +0200

    Form updated

--- js/main.js
+++ js/main.js
@@ -16,3 +16,18 @@ const observer = new IntersectionObserver(
 sections.forEach(section => {
   observer.observe(section);
 });
+
+function sendMail(event) {
+  event.preventDefault();
+
+  const message = document.querySelector('[name="message"]').value.trim();
+
+  const subject = encodeURIComponent("Contact naar Bram Grippeling");
+  const body = encodeURIComponent(`${message}`);
+  const recipient = "38999@ma-web.nl";
+
+  const mailtoLink = `mailto:${recipient}?subject=${subject}&body=${body}`;
+
+  // Open user's email app
+  window.location.href = mailtoLink;
+}
