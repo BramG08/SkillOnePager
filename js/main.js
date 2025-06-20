eb68573386652f0f433a04ebcf0cac7646f7d77d
BramG08 <bgrippeling08@gmail.com>
2025-06-20 14:46:43 +0200

    Updated javascript

--- js/main.js
+++ js/main.js
@@ -20,13 +20,15 @@ sections.forEach(section => {
 function sendMail(event) {
   event.preventDefault();
 
+  const send_to_mail = document.querySelector('[name="send_to_mail"]').value.trim();
+  const email = document.querySelector('[name="email"]').value.trim();
   const message = document.querySelector('[name="message"]').value.trim();
-
   const subject = encodeURIComponent("Contact naar Bram Grippeling");
+  
   const body = encodeURIComponent(`${message}`);
-  const recipient = "38999@ma-web.nl";
+  const recipient = encodeURIComponent(`${email}`);
 
-  const mailtoLink = `mailto:${recipient}?subject=${subject}&body=${body}`;
+  const mailtoLink = `mailto:${send_to_mail}&?cc=${recipient}&?subject=${subject}&body=${body}`;
 
   // Open user's email app
   window.location.href = mailtoLink;
